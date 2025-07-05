d78be799d797767e4eed0f431ffe13cc888e8a7b
Rahul Swain <level432520537352822@gmail.com>
2025-07-05 10:11:36 +0530

    modified and fixed the error containing stuff

--- byte-sized-banishment/client/src/App.jsx
+++ byte-sized-banishment/client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Toaster position="top-center" reverseOrder={false} />
       <Router>
         <Routes>
@@ -21,7 +22,7 @@ function App() {
           />
         </Routes>
       </Router>
-    </>
+    </AuthProvider>
   );
 }
 
